30711e32c0e759907197666aa7e3e818d8ce8b38
Your Name <you@example.com>
2026-05-06 20:08:41 +0100

    p2 done.

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -6,5 +6,5 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 644 --n
 # Wait for K3s to be fully ready
 sleep 10
 
-# Apply all app configurations from the confs folder
+# Deploy all Kubernetes manifests from the confs directory
 kubectl apply -f /vagrant/confs/
